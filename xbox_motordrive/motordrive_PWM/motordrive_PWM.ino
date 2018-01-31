008009c4a8b1fd92685c0f802afb9f928240e156
cfuhrman-utcr2018 <cfuhrman@citadel.edu>
2018-01-31 14:57:25 -0500

    Code compiled. Learned that std_msgs/UInt16 message is a wrapper around an integer type, not an integer type. I accessed the data by using var.data

--- xbox_motordrive/motordrive_PWM/motordrive_PWM.ino
+++ xbox_motordrive/motordrive_PWM/motordrive_PWM.ino
@@ -1,27 +1,24 @@
 #include <ros.h>
 #include <std_msgs/Float64.h>
-#include <std_msgs/UInt32.h>
+#include <std_msgs/UInt16.h>
+#include <stdlib.h>
 
-int Left_Motor = 10;
-int Right_Motor = 11;
+const int Left_Motor = 10;
+const int Right_Motor = 11;
 
 
 ros::NodeHandle nh_arduino; // Initiated the node handle
 
- 
-
-void duty_left (const std_msgs::Float64& duty_cycle_L){
-  int duty = duty_left;
-  analogWrite(Left_Motor, duty);
+void duty_left (const std_msgs::UInt16& duty_cycle_L){
+  analogWrite(Left_Motor, duty_cycle_L.data);
 }
 
-void duty_right (const std_msgs::Float64& duty_cycle_R){
-  int duty = duty_right;
-  analogWrite(Right_Motor, duty);
+void duty_right (const std_msgs::UInt16& duty_cycle_R){
+  analogWrite(Right_Motor, duty_cycle_R.data);
 }
 
-ros::Subscriber<std_msgs::Float64> sub_D_L("Duty_Cycle_Left/command", duty_left);
-ros::Subscriber<std_msgs::Float64> sub_D_R("Duty_Cycle_Right/command", duty_right);
+ros::Subscriber<std_msgs::UInt16> sub_D_L("Duty_Cycle_Left/command", duty_left);
+ros::Subscriber<std_msgs::UInt16> sub_D_R("Duty_Cycle_Right/command", duty_right);
 
 
 void setup() {
@@ -35,5 +32,4 @@ void setup() {
 
 void loop() {
   nh_arduino.spinOnce();
-
 }
